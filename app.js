230ef4633197126d3a3d03d25b430e83c8b9ecc3
Daniel Oliveira <93006317+danieloliveiraaa@users.noreply.github.com>
2022-02-02 01:35:45 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+//console.log("Daniel") - Exibir mensangem
+/*
+let idade = 5;
+console.log(idade);
+
+let pixels = 1200 //variavel que pode ser alterada.
+console.log(pixels);
+
+const valorIngressoAdulto = 20; //variavel constante.
+console.log("Valor do ingresso para adultos: " +valorIngressoAdulto);
+
+
+let nome = 'Daniel';
+let idade = 25;
+let estaAprovado = true;
+
+//OBJETO
+let pessoa = {
+    nome: 'Daniel',
+    idade: 25,
+    estaAprovado = true
+};
+
+
+console.log(pessoa);
+*/
+
+
+
+//Arrays
+/*
+let familia = [40,42,20,30];
+
+console.log(familia.length);
+
+console.log(familia[0]);
+
+
+let nomeColega = ['Kaique','Gustavo'];
+
+console.log(nomeColega[1]);
+*/
+
+
+
+
+/*
+funções
+let corSite = "azul";
+
+function resetaCor(cor,tonalidade){
+    corSite = cor + tonalidade;
+};
+
+console.log(corSite);
+resetaCor("verde"," claro");
+console.log(corSite);
+
+
+
